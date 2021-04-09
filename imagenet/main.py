7d432de01a4b33a13d1a15518b8d1dc6cfe988dc
vuducquan96 <vuducquan96@gmail.com>
2021-04-09 15:58:57 +0900

    [REPLACE] imagenet with cifar10

--- imagenet/main.py
+++ imagenet/main.py
@@ -197,6 +197,15 @@ def main_worker(gpu, ngpus_per_node, args):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
+    transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=True, transform=transform) 
+    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=True, transform=transform)
     train_dataset = datasets.ImageFolder(
         traindir,
         transforms.Compose([
@@ -212,18 +221,10 @@ def main_worker(gpu, ngpus_per_node, args):
         train_sampler = None
 
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        train_set, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
-    val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+    val_loader = DataLoader(dataset=test_set, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True)
 
     if args.evaluate:
         validate(val_loader, model, criterion, args)
